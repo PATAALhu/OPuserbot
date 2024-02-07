edbef33079ff76a8e313d6425577e917d21e7d8a
PATAALhu <158342564+PATAALhu@users.noreply.github.com>
2024-02-07 16:38:17 +0530

    Update alive.py

--- SpamX/modules/for_id/alive.py
+++ SpamX/modules/for_id/alive.py
@@ -106,8 +106,8 @@ async def SpamX_(SpamX: Client, message: Message):
        spamx_reply += f"**Total Sudos:** `{users_db.sudo_count()}` \n"
        spamx_reply += f"**Total Gbanned users:** `{gban_db.gban_count()}` \n"
     spamx_reply += "<=======================> \n\n"
-    spamx_reply += "**Source Code:** [GitHub 🐈‍⬛](https://github.com/RiZoeLX/SpamX) \n\n"
-    spamx_reply += "**© @RiZoeLX** || Support: @DNHxHELL"
+    spamx_reply += "**Source Code:** [OWNER](https://t.me/PATAAL_NAGRI) \n\n"
+    spamx_reply += "**© @Ditto_999** || Support: @PATAAL_NAGRI"
     
     try:
        await info.edit_text(spamx_reply, disable_web_page_preview=True)
